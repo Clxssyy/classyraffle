23180980487ba648ae1b8fa31c328aa49b50e1ba
Michael Connolly <mpc63@uakron.edu>
2023-09-05 20:36:18 -0400

    Add enter button > participant count functionality

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,18 +1,31 @@
-const { Events } = require('discord.js');
+const { Events, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    name: Events.InteractionCreate,
-    async execute(interaction) {
-        if (!interaction.isChatInputCommand()) return;
-
+  name: Events.InteractionCreate,
+  async execute(interaction) {
+    if (!interaction.isChatInputCommand() && !interaction.isButton()) return;
+    try {
+      if (interaction.isChatInputCommand()) {
         const command = interaction.client.commands.get(
-            interaction.commandName
+          interaction.commandName
         );
 
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error('[ERROR] ', error);
+        command.execute(interaction);
+      }
+      if (interaction.isButton()) {
+        if (interaction.customId === 'enter_giveaway') {
+          const oldEmbed = interaction.message.embeds[0];
+
+          const fields = oldEmbed.fields;
+          fields[2].value = String(Number(fields[2].value) + 1);
+
+          const newEmbed = EmbedBuilder.from(oldEmbed).setFields(fields);
+
+          interaction.message.edit({ embeds: [newEmbed] });
         }
-    },
+      }
+    } catch (error) {
+      console.error('[ERROR] ', error);
+    }
+  },
 };
